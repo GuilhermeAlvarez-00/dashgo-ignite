81993248b635c44017fdf33891c4d51693438aa8
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-08 17:40:46 -0300

    criando pagina sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,67 @@
 import type { NextPage } from 'next'
+import { Button, Flex, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   return (
-    <h2>Hello World</h2>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              focusBorderColor="pink.500" 
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />  
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              name="senha"
+              focusBorderColor="pink.500" 
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          >
+            Entrar
+          </Button>
+      </Flex>
+    </Flex>
   )
 }
 
