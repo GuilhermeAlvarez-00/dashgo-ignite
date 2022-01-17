031b8f2a045682a4b3794075aa1bf42ac8bc2e75
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-01-16 21:22:23 -0300

    alterando espaçamento do dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -68,13 +68,28 @@ export default function Dashboard() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
-          <Box p="8" bg="gray.800" borderRadius="8" pb="4">
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          align="flex-start"
+        >
+          <Box
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius="8"
+            pb="4"
+          >
             <Text fontSize="lg" mb="4">Inscritos da semana</Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
 
-          <Box p="8" bg="gray.800" borderRadius="8" pb="4">
+          <Box
+            p={["6", "8"]}
+            bg="gray.800"
+            borderRadius="8"
+            pb="4"
+          >
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
