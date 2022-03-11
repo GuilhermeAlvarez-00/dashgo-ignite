5139909d69bf7fceef5ca0cfa93e2703494fd779
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-11 20:26:09 -0300

    refatorando tudo

--- context/SidebarDrawerContext.tsx
+++ context/SidebarDrawerContext.tsx
@@ -1,23 +1,24 @@
-import { createContext, useContext, ReactNode, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { createContext, ReactNode, useContext, useEffect } from 'react'
 import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 interface SidebarDrawerProviderProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
 type SidebarDrawerContextData = UseDisclosureReturn
 
 const SidebarDrawerContext = createContext({} as SidebarDrawerContextData)
 
-export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
+export function SidebarDrawerProvider({
+  children,
+}: SidebarDrawerProviderProps) {
   const disclosure = useDisclosure()
-
   const router = useRouter()
 
   useEffect(() => {
     disclosure.onClose()
-  }, [router.asPath, disclosure])
+  }, [router.asPath])
 
   return (
     <SidebarDrawerContext.Provider value={disclosure}>
