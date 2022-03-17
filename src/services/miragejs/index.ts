94e0d3a4f42f631c128ed3fc67db8f1e4e6ca9bd
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-17 10:00:41 -0300

    configurando paginaçao no miragejs

--- src/services/miragejs/index.ts
+++ src/services/miragejs/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import { faker } from '@faker-js/faker'
 
 type User = {
@@ -35,7 +35,20 @@ export function makeServer() {
       this.namespace = 'api'
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 5 } = request.queryParams
+
+        const total = schema.all('user').length
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        )
+
+        return new Response(200, { 'x-total-count': String(total) }, { users })
+      })
       this.post('/users')
 
       this.namespace = ''
