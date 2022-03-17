6872cc80b1859dfa95fe8c856e17415d0a5a4f57
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-17 07:33:05 -0300

    adicionando miragejs para fake api

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -20,12 +20,19 @@ import Link from 'next/link'
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
+import { useEffect } from 'react'
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  }, [])
 
   return (
     <Box>
