17dc0f71197199b1556f5f2eb494630fca8a6beb
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-03-17 08:55:59 -0300

    usando o ReactQueryDevTools e definindo staleTime

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,18 +25,24 @@ import { Sidebar } from '../../components/Sidebar'
 import { useEffect } from 'react'
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
-    const users = data.users.map((user) => ({
-      ...user,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-        dateStyle: 'long',
-      }),
-    }))
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users')
+      const data = await response.json()
+      const users = data.users.map((user) => ({
+        ...user,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          dateStyle: 'long',
+        }),
+      }))
 
-    return users
-  })
+      return users
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  )
 
   const isWideVersion = useBreakpointValue({
     base: false,
